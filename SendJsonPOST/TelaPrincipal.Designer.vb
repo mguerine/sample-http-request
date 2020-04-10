bbc329d06259f4f2b1b86e889c66d7ca6867a31c
Marcos A. Guerine <marcosgribeiro@gmail.com>
2020-04-10 16:28:07 -0300

    send-receive http request sample

---
+++ SendJsonPOST/TelaPrincipal.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TelaPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BTSend = New System.Windows.Forms.Button()
+        Me.BTReceive = New System.Windows.Forms.Button()
+        Me.TBMatricula = New System.Windows.Forms.TextBox()
+        Me.TBNome = New System.Windows.Forms.TextBox()
+        Me.TBEmail = New System.Windows.Forms.TextBox()
+        Me.TBSaldo = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BTSend
+        '
+        Me.BTSend.Location = New System.Drawing.Point(80, 163)
+        Me.BTSend.Name = "BTSend"
+        Me.BTSend.Size = New System.Drawing.Size(75, 23)
+        Me.BTSend.TabIndex = 0
+        Me.BTSend.Text = "Send"
+        Me.BTSend.UseVisualStyleBackColor = True
+        '
+        'BTReceive
+        '
+        Me.BTReceive.Location = New System.Drawing.Point(238, 163)
+        Me.BTReceive.Name = "BTReceive"
+        Me.BTReceive.Size = New System.Drawing.Size(75, 23)
+        Me.BTReceive.TabIndex = 1
+        Me.BTReceive.Text = "Receive"
+        Me.BTReceive.UseVisualStyleBackColor = True
+        '
+        'TBMatricula
+        '
+        Me.TBMatricula.Location = New System.Drawing.Point(70, 45)
+        Me.TBMatricula.Name = "TBMatricula"
+        Me.TBMatricula.Size = New System.Drawing.Size(100, 20)
+        Me.TBMatricula.TabIndex = 2
+        '
+        'TBNome
+        '
+        Me.TBNome.Location = New System.Drawing.Point(70, 71)
+        Me.TBNome.Name = "TBNome"
+        Me.TBNome.Size = New System.Drawing.Size(100, 20)
+        Me.TBNome.TabIndex = 3
+        '
+        'TBEmail
+        '
+        Me.TBEmail.Location = New System.Drawing.Point(70, 97)
+        Me.TBEmail.Name = "TBEmail"
+        Me.TBEmail.Size = New System.Drawing.Size(100, 20)
+        Me.TBEmail.TabIndex = 4
+        '
+        'TBSaldo
+        '
+        Me.TBSaldo.Location = New System.Drawing.Point(70, 123)
+        Me.TBSaldo.Name = "TBSaldo"
+        Me.TBSaldo.Size = New System.Drawing.Size(100, 20)
+        Me.TBSaldo.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Matricula"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Nome"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Email"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 126)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Saldo"
+        '
+        'TelaPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 199)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TBSaldo)
+        Me.Controls.Add(Me.TBEmail)
+        Me.Controls.Add(Me.TBNome)
+        Me.Controls.Add(Me.TBMatricula)
+        Me.Controls.Add(Me.BTReceive)
+        Me.Controls.Add(Me.BTSend)
+        Me.Name = "TelaPrincipal"
+        Me.Text = "SendReceive JSON API"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BTSend As Button
+    Friend WithEvents BTReceive As Button
+    Friend WithEvents TBMatricula As TextBox
+    Friend WithEvents TBNome As TextBox
+    Friend WithEvents TBEmail As TextBox
+    Friend WithEvents TBSaldo As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ SendJsonPOST/TelaPrincipal.vb
@@ -0,0 +1,123 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Linq
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class TelaPrincipal
+
+    Private Function ReceiveResponse(uri As Uri, contentType As String, method As String) As String
+        'Criando as variáveis usadas na criação da requisição HTTP
+        Dim request As HttpWebRequest
+        Dim response As HttpWebResponse = Nothing
+        Dim reader As StreamReader
+        Dim responseString As String
+
+        responseString = ""
+
+        Try
+            'Construindo a requisição HTTP com a rota da URL passada
+            request = DirectCast(WebRequest.Create(uri), HttpWebRequest)
+            'Fazendo a requisição e coletando a resposta
+            response = DirectCast(request.GetResponse(), HttpWebResponse)
+            'Lendo a resposta à requisição
+            reader = New StreamReader(response.GetResponseStream())
+
+            'Transformando a resposta em string para facilitar a leitura do JSON
+            Dim rawresp As String
+            rawresp = reader.ReadToEnd()
+
+            'Fazendo o parse da string
+            Dim array As JArray = JArray.Parse(rawresp)
+
+            'Imprimindo no console os itens encontrados no array
+            For Each item As JObject In array
+                responseString += item.ToString
+                Console.WriteLine(item)
+
+            Next
+
+        Catch ex As Exception
+            'Em caso de erro, abrir uma messagebox
+            MsgBox(ex.ToString)
+        Finally
+            If Not response Is Nothing Then
+                response.Close()
+            End If
+        End Try
+
+        Return responseString
+    End Function
+
+
+    Private Function SendRequest(uri As Uri, jsonDataBytes As Byte(), contentType As String, method As String) As String
+        Dim response As String
+        Dim request As WebRequest
+
+        'Construindo a requisição HTTP com a rota da URL passada
+        request = WebRequest.Create(uri)
+        'Criando o objeto JSON da requisição
+        request.ContentLength = jsonDataBytes.Length
+        request.ContentType = contentType
+        request.Method = method
+
+        'Fazendo a escrita das informações no server via requisição HTTP POST
+        Using requestStream = request.GetRequestStream
+            requestStream.Write(jsonDataBytes, 0, jsonDataBytes.Length)
+            requestStream.Close()
+
+            'Capturando a resposta do server após escrita
+            Using responseStream = request.GetResponse.GetResponseStream
+                Using reader As New StreamReader(responseStream)
+                    response = reader.ReadToEnd()
+                End Using
+            End Using
+        End Using
+
+        Return response
+    End Function
+
+    Private Sub BTSend_Click(sender As Object, e As EventArgs) Handles BTSend.Click
+        'String JSON usada para cadastro de usuário
+        Dim jsonString As String
+        '{
+        '"matricula":"$matricula",
+        '"nome":"nome",
+        '"email":"email",
+        '"saldo":0
+        '}
+
+        'Construindo a string com os valores preenchidos no formulário
+        '** Não fiz validação de dados!! Isso deve ser feito na versão principal do S.I.S.T.E.MA. !!
+        jsonString = "{""matricula"":""" &
+                        TBMatricula.Text &
+                        """,""nome"":""" &
+                        TBNome.Text &
+                        """,""email"":""" &
+                        TBEmail.Text &
+                        """,""saldo"":" &
+                        TBSaldo.Text & "}"
+
+        'URL para a rota de criação de usuários (definida na API pelo Erick)
+        Dim myUri As New Uri("https://sistemaifrj.herokuapp.com/users/")
+
+        'Codificando a string JSON para ser enviada na requisição HTTP do tipo POST
+        Dim data = Encoding.UTF8.GetBytes(jsonString)
+        Dim result_post = SendRequest(myUri, data, "application/json", "POST")
+        Console.WriteLine(result_post)
+
+    End Sub
+
+    Private Sub BTReceive_Click(sender As Object, e As EventArgs) Handles BTReceive.Click
+        'URL para rota de lista de usuários (definida na API pelo Erick)
+        Dim myUri As New Uri("https://sistemaifrj.herokuapp.com/users/")
+
+        'Usando a função ReceiveResponse para buscar os usuários cadastrados. Usando o método GET para a requisição HTTP
+        Dim result_post = ReceiveResponse(myUri, "application/json", "GET")
+
+        'Imprimindo no console o resultado da busca
+        Console.WriteLine(result_post)
+
+    End Sub
+End Class
